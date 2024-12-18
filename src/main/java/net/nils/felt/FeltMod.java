712dfeeed87f602a7ae68ecc3323b4d87d0ba81a
FrostByte02 <nils_schulz@magenta.de>
2024-12-18 22:12:28 +0100

    Initial commit

---
+++ src/main/java/net/nils/felt/FeltMod.java
@@ -0,0 +1,16 @@
+package net.nils.felt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FeltMod implements ModInitializer {
+	public static final String MOD_ID = "felt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nils/felt/FeltModClient.java
@@ -0,0 +1,10 @@
+package net.nils.felt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FeltModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
